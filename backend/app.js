d8d7cdbedf75b5d567b3d19f4b1bfdbce2ea1dcb
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-02-23 20:36:49 +0700

    first commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
+app.use(fileUpload());
+
+// config
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+ })}
+
+// Route import
+const product = require("./routes/ProductRoute");
+const user = require("./routes/UserRoute");
+const order = require("./routes/OrderRoute");
+const payment = require("./routes/PaymentRoute");
+
+app.use("/api/v2", product);
+
+app.use("/api/v2", user);
+
+app.use("/api/v2", order);
+
+app.use("/api/v2", payment);
+
+// It's for errorHanderling
+app.use(ErrorHandler);
+
+module.exports = app
